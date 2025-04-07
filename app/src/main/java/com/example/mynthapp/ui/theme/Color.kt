f4d04ab0553c1078fa3db7a9fdc944b8a71bd320
DyllonWright <djw0026@uah.edu>
2025-04-07 12:02:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/mynthapp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.mynth.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Obsidian-inspired colors
+val ObsidianDark = Color(0xFF1E1E2E) // Very dark gray/black
+val ObsidianPurple = Color(0xFF9463F2) // A vibrant purple
+val ObsidianLightPurple = Color(0xFFA080F0) // A lighter, softer purple
+val ObsidianGray = Color(0xFF2E2E3E) // A dark Gray
+
+val Purple80 = ObsidianLightPurple
+val PurpleGrey80 = ObsidianGray
+val Pink80 = ObsidianPurple
+
+val Purple40 = ObsidianPurple
+val PurpleGrey40 = ObsidianGray
+val Pink40 = ObsidianDark
